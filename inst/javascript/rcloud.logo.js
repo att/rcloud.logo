a4c9cb60d4e6f56f5d35bc5f0461eca07119fbe2
Gordon Woodhull <gordon@woodhull.com>
2015-02-12 17:25:17 -0500

    adjust paddings/margins
    fixes #1

--- inst/javascript/rcloud.logo.js
+++ inst/javascript/rcloud.logo.js
@@ -7,7 +7,7 @@ return {
                 area: 'header',
                 sort: 1000,
                 create: function() {
-                    return $('<span style="display: inline-block; overflow: hidden; width: 100; height: 50"><img style="position: relative; top: -75px; left: -25px; width: 150px; height: 200px;" src="shared.R/rcloud.logo/RCloud_Color_Vector.svg"></span>');
+                    return $('<span style="display: inline-block; overflow: hidden; width: 100; height: 50; margin-right: 1em;"><img style="position: relative; top: -75px; left: -25px; width: 150px; height: 200px;" src="shared.R/rcloud.logo/RCloud_Color_Vector.svg"></span>');
                 }
             }
         });
@@ -17,7 +17,7 @@ return {
                     area: 'header',
                     sort: 2000,
                     create: function() {
-                        return $('<a class="navbar-brand" style="float: none; position:relative; bottom: 17px; color: #999999; padding: 0; font-size: 20px" href="#"><span style="display: inline-block; overflow: hidden; position: relative; bottom: -2px"><img width=20 height=20 src="shared.R/rcloud.logo/dots.svg"></span>&nbsp' + deployment + '</a>');
+                        return $('<a class="navbar-brand" style="float: none; position:relative; bottom: 17px; color: #999999; padding: 0; padding-right: 0.5em; font-size: 20px" href="#"><span style="display: inline-block; overflow: hidden; position: relative; bottom: -2px"><img width=20 height=20 src="shared.R/rcloud.logo/dots.svg"></span>&nbsp' + deployment + '</a>');
                     }
                 }
             });
